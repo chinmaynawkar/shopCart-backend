bb16ccdb9a8121461e13844c64a5c3b47db5c8ae
chinmaynawkar <119050434+chinmaynawkar@users.noreply.github.com>
2024-02-19 11:14:08 +0530

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://cnawkar19:eKrc13oL6L21fY7z@mydatabase.ao4wrdn.mongodb.net/shopcartDB?retryWrites=true&w=majority');
+        await mongoose.connect('Your URL');
             
         console.log("MongoDB connected");
 
